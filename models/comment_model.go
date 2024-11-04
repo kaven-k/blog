4e092a57ee13889d09f0b6561b1b27c9bd751ee4
lanyangyangdawang <2902772790@qq.com>
2024-11-04 15:27:05 +0800

    表结构迁移

--- models/comment_model.go
+++ models/comment_model.go
@@ -11,7 +11,7 @@ type CommentModel struct {
 	CommentCount       int             `gorm:"size:8;default:0;" json:"comment_count"`          // 子评论数
 	//Article            ArticleModel    `gorm:"foreignKey:ArticleID" json:"-"`                   // 关联的文章
 	//ArticleID uint      `json:"article_id"` // 文章id
-	ArticleEsID string    `json:"article_es_id"` // 文章id
-	User        UserModel `json:"user"`          //关联的用户
-	UserID      uint      `json:"user_id"`       // 评论的用户
+	ArticleID string    `gorm:"size:32" json:"article_id"` // 文章id
+	User      UserModel `json:"user"`                      //关联的用户
+	UserID    uint      `json:"user_id"`                   // 评论的用户
 }
